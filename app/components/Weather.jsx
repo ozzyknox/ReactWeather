790c47881c18173e18b2cf280b49856b15a2532e
ozzyawaken <ozzy.knox@awakensoftware.com>
2017-01-31 21:07:38 +0100

    Add styles to wether page

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -31,14 +31,15 @@ var Weather = React.createClass({
         function renderMessage() {
             debugger;
             if (isLoading) {
-                return <h3>Fetching weather...</h3>
+                return <h3 className="text-center">Fetching weather...</h3>
             } else if (temp && location) {
                 return <WeatherMessage location={location} temp={temp}/>;
             }
         }
 
         return (
-            <div style={{textAlign: 'center'}}>
+            <div>
+                <h1 className="text-center">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {renderMessage()}
             </div>
